9e6a2d5036eeb7ea5d2a946572ff2abffdb5a554
saccdev12 <chaconsebastian830@gmail.com>
2023-08-14 16:01:46 -0300

    minor changes

--- abstractclass.dart
+++ abstractclass.dart
@@ -1,14 +1,23 @@
 void main() {
   final windPlant = WindPlant(initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);
 
-  print(windPlant);
+  print('wind: ${chargePhone(windPlant)}');
+  print('nuclear: ${chargePhone(nuclearPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy');
+  }
+  return plant.energyLeft - 10;
 }
 
 enum PlantType { nuclear, wind, water }
 
 abstract class EnergyPlant {
   double energyLeft;
-  PlantType type;
+  final PlantType type;
 
   EnergyPlant({required this.energyLeft, required this.type});
 
@@ -22,5 +31,20 @@ class WindPlant extends EnergyPlant {
   @override
   void consumeEnergy(double amount) {
     energyLeft -= amount;
+  }
+}
+
+class NuclearPlant implements EnergyPlant {
+  @override
+  double energyLeft;
+
+  @override
+  final PlantType type = PlantType.nuclear;
+
+  NuclearPlant({required this.energyLeft});
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= (amount * 0.5);
   }
 }
